beb756897d4b21e37a9120049dfc069eecbfb2ab
Shantu Das <ajshantudas@gmail.com>
2018-10-11 16:41:31 +0600

    article response updated

--- app/src/main/java/com/muvasia/driver/articleapp/response/ArticleResponse.kt
+++ app/src/main/java/com/muvasia/driver/articleapp/response/ArticleResponse.kt
@@ -3,14 +3,6 @@ package com.muvasia.driver.articleapp.response
 import com.google.gson.annotations.SerializedName
 import com.muvasia.driver.articleapp.Model.Article
 
-/*class ArticleResponse{
-
-    @SerializedName("title")
-    var title: String? = null
-
-
-}*/
-
 class ArticleResponse {
 
     @SerializedName("data")
